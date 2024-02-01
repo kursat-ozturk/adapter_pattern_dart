53941557f6243edb755a32e9e2369944cf8af6c2
Kürşat Öztürk <kursatsoftware@gmail.com>
2024-02-01 12:46:43 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/kratapp/adapter_pattern_dart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.kratapp.adapter_pattern_dart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list_with_separator.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'contact.dart';
+
+class XMLLongListWithSeparator extends StatelessWidget {
+  final List<Contact> listOfItems = XmlContactsAdapter().getContacts();
+  XMLLongListWithSeparator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.separated(
+        itemCount: listOfItems.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            onTap: () {
+              if (kDebugMode) {
+                print('Clicked on item #$index');
+              } // Print to console
+            },
+            title: Text(listOfItems[index].fullName),
+            subtitle: Text('${listOfItems[index].email} ${listOfItems[index].phoneNumber}'),
+            leading: Container(
+              height: 50,
+              width: 50,
+              color: Colors.amber,
+            ),
+            trailing: const Icon(Icons.edit),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) {
+          return const Divider();
+        },
+      ),
+    );
+  }
+}
